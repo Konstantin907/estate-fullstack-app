9571a16897e511f10059859a4f687c9bc7dd43a1
Konstantin <konstantinivanovic12345@gmail.com>
2024-06-09 19:28:58 -0700

    auth routes with db operations added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async(req, res)=>{
     const { username, email, password } = req.body;
@@ -26,11 +27,45 @@ export const register = async(req, res)=>{
     
 }
 
-export const login = (req, res)=>{
-    //db operations
+export const login = async(req, res)=>{
+    const { username, password } = req.body;
+try {
+    //if user exist or not:
+    const user =await prisma.user.findUnique({
+        where:{username}
+    });
+    if(!user){
+        return res.status(401).json({message: 'Invalid credentials!'})
+    }
+    //check password:
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).json({message: 'Invalid credentials!'})
+    }
+    //generate token and send it to user:
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign({
+        id: user.id
+    }, process.env.JWT_SECRET_KEY,
+    {expiresIn: age})
+
+   
+    res.cookie("token", token, {
+        httpOnly:true,
+        //secure: true
+        maxAge: age,
+    }).status(200).json({message: 'Login successfull!'})
+} catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Failed to login!' })
+}
+    
 }
 
 
 export const logout = (req, res)=>{
-    //db operations
+    res.clearCookie("token").status(200).json({
+        message: "Logout successfull!"
+    })
 }
