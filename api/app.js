0c43431eeee80641a7b79b2eaa81ac32bf7c6e92
Konstantin Ivanovic <konstantinivanovic12345@gmail.com>
2025-10-09 17:38:26 -0700

    deploy api

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+app.use(cors({  origin: ["http://localhost:5173", "https://estate-fullstack-app.vercel.app"], credentials: true}))
 
 app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
