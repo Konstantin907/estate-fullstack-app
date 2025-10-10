df9e7e8098e6e31572c63bf52eea47e8a9b49269
Konstantin Ivanovic <konstantinivanovic12345@gmail.com>
2025-10-09 19:53:17 -0700

    api mistake

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiRequest = axios.create({
-    baseURL: import.meta.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 })
 
