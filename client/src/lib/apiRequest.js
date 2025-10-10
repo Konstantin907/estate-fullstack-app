6456ffe819d0f2ea346db96b9302824411217fdb
Konstantin Ivanovic <konstantinivanovic12345@gmail.com>
2025-10-09 19:58:45 -0700

    api test

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+
 const apiRequest = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
