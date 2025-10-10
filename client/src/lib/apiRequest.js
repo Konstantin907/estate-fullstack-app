91af7af1bcc0e70f06cb49a1f2f62abadab639dd
Konstantin Ivanovic <konstantinivanovic12345@gmail.com>
2025-10-09 17:32:29 -0700

    deploy prep

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const apiRequest = axios.create({
-    baseURL: 'http://localhost:8800/api',
+    baseURL: import.meta.VITE_API_URL || 'http://localhost:8800/api',
     withCredentials: true,
 })
 
