d96ec73adf14ad2462693ee6177b8e3dfd4e7a32
Konstantin Ivanovic <konstantinivanovic12345@gmail.com>
2025-10-30 13:21:02 -0700

    adding ping to keep mongo db alive

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -2,13 +2,21 @@ import {PrismaClient} from "@prisma/client"
 
 const prisma = new PrismaClient();
 
-// connect check:
 async function testDB() {
   try {
-    await prisma.$connect()
-    console.log("Database connected successfully")
+    await prisma.$connect();
+    console.log("Database connected successfully");
+
+    setInterval(async () => {
+      try {
+        await prisma.$runCommandRaw({ ping: 1 });
+        console.log("🔁 Pinged Mongo cluster to keep it alive");
+      } catch (err) {
+        console.error("Ping failed:", err.message);
+      }
+    }, 30 * 60 * 1000);
   } catch (err) {
-    console.error("Database connection failed:", err)
+    console.error("Database connection failed:", err);
   }
 }
 testDB()
